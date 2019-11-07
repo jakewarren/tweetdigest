080895a08f2f9b4a54faca650e4cee851e34a76f
Jake Warren <jakewarren@users.noreply.github.com>
2019-11-07 11:04:59 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,362 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"fmt"
+	"html/template"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/jakewarren/metascraper"
+	apppaths "github.com/muesli/go-app-paths"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"gopkg.in/gomail.v2"
+)
+
+type app struct {
+	Client *anaconda.TwitterApi
+	Config struct {
+		Threshold  time.Duration
+		ConfigFile string
+	}
+}
+
+func main() {
+	a := app{}
+
+	log.Logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().Caller().Timestamp().Logger()
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	pflag.StringVarP(&a.Config.ConfigFile, "config", "c", "", "filepath to the config file")
+	pflag.DurationVarP(&a.Config.Threshold, "duration", "d", 0, "how far back to include tweets in the digest (example: \"-24h\")")
+	pflag.Parse()
+
+	// check for required args
+	if pflag.NArg() < 1 {
+		log.Fatal().Msg("twitter screenname was not provided")
+	}
+	if a.Config.Threshold == 0 {
+		log.Fatal().Msg("threshold duration was not provided")
+	}
+
+	// load up config
+	if a.Config.ConfigFile != "" {
+		viper.SetConfigFile(a.Config.ConfigFile)
+	} else {
+		viper.SetConfigName("tweetdigest") // name of config file (without extension)
+		viper.AddConfigPath(".")           // optionally look for config in the working directory
+		viper.AddConfigPath("$HOME")       // optionally look for config in home directory
+
+		userScope := apppaths.NewScope(apppaths.User, "", "tweetdigest")
+		xdgConfigDir, configDirErr := userScope.ConfigPath("")
+		if configDirErr == nil {
+			viper.AddConfigPath(xdgConfigDir)
+		}
+	}
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		log.Fatal().Err(err).Msg("Fatal error config file")
+	}
+
+	// init Twitter API
+	anaconda.SetConsumerKey(viper.GetString("consumer_key"))
+	anaconda.SetConsumerSecret(viper.GetString("consumer_secret"))
+	a.Client = anaconda.NewTwitterApi(viper.GetString("access_token"), viper.GetString("access_token_secret"))
+
+	username := pflag.Arg(0)
+	tweets := a.getTweetsForUser(username)
+
+	if len(tweets) == 0 {
+		return
+	}
+
+	m := gomail.NewMessage()
+	m.SetAddressHeader("From", viper.GetString("email_from.address"), viper.GetString("email_from.name"))
+	m.SetHeader("To", viper.GetStringSlice("email_to")...)
+	m.SetHeader("Subject", fmt.Sprintf("@%s Tweet Digest for %s", username, time.Now().Format("1/2/06")))
+	m.SetBody("text/html", a.generateHTML(tweets))
+	d := gomail.Dialer{Host: "localhost", Port: 25}
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+}
+
+func (a app) getTweetsForUser(s string) []anaconda.Tweet {
+	v := url.Values{}
+	v.Set("screen_name", s)
+	v.Set("count", "50")
+
+	var timeline []anaconda.Tweet
+
+	// for some reason Twitter will occasionally only return one tweet, so use this hacky retry method
+	for i := 1; i <= 3; i++ {
+		var err error
+		timeline, err = a.Client.GetUserTimeline(v)
+		if err != nil {
+			log.Error().Err(err).Msg("error getting timeline")
+		}
+
+		tweetCount := len(timeline)
+		log.Debug().Int("tweet count", tweetCount).Int("attempt", i).Msg("pulled down tweets")
+		if tweetCount > 1 {
+			break
+		}
+		log.Debug().Msgf("retrying. sleeping for %d seconds...", i*30)
+		time.Sleep(time.Duration(i*30) * time.Second)
+	}
+
+	dateThreshold := time.Now().Local().Add(a.Config.Threshold)
+
+	tweets := make([]anaconda.Tweet, 0)
+
+	for _, tweet := range timeline {
+
+		cTime, _ := tweet.CreatedAtTime()
+		cTime = cTime.Local() // convert to local timezone
+
+		if cTime.After(dateThreshold) {
+			tweets = append(tweets, tweet)
+		}
+
+	}
+
+	return tweets
+}
+
+type emailBody struct {
+	Tweets []anaconda.Tweet
+}
+
+func (a app) generateHTML(tweets []anaconda.Tweet) string {
+	var (
+		e   emailBody
+		err error
+	)
+	e.Tweets = tweets
+
+	funcMap := template.FuncMap{
+		"formatTime": func(t anaconda.Tweet) template.HTML {
+			// get the creation time and convert to user's local timezone
+			cTime, _ := t.CreatedAtTime()
+			cTime = cTime.Local()
+			return template.HTML(cTime.Format("Jan 2"))
+		},
+		"getTwitterImage": func(url string) template.HTML {
+			p, metaErr := metascraper.Scrape(url)
+			if metaErr != nil {
+				// log.Error().Str("url", url).Err(metaErr).Msg("error getting metadata for an url")
+				return ""
+			}
+
+			for _, m := range p.MetaData() {
+				if m.Name == "twitter:image" || m.Name == "og:image" {
+					return template.HTML(fmt.Sprintf(`<img src="%s" style="max-width:100%%; padding-bottom:5px">`, m.Content))
+				}
+			}
+
+			return ""
+		},
+	}
+
+	t := template.New("emailTmpl").Funcs(funcMap)
+	if t, err = t.Parse(emailTemplate); err != nil {
+		panic(err)
+	}
+	var buf bytes.Buffer
+	if err = t.Execute(&buf, e); err != nil {
+		log.Error().Err(err).Msg("error executing template")
+	}
+
+	return buf.String()
+}
+
+const emailTemplate = `
+<html xmlns="http://www.w3.org/1999/xhtml"
+    style='box-sizing:border-box; font-family:"Helvetica Neue", Helvetica, Arial, sans-serif'>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8=">
+    <meta name="viewport" content="width=device-width">
+    <title></title>
+
+
+</head>
+
+<body style="height:100%; margin:0; width:100%; background-color:#fff" height="100%" width="100%" bgcolor="#ffffff">
+
+    <style type="text/css">
+        @media only screen and (max-width: 640px) {
+            body {
+                padding: 0 !important
+            }
+
+            h1,
+            h2,
+            h3,
+            h4 {
+                font-weight: 800 !important;
+                margin: 20px 0 5px !important
+            }
+
+            h1 {
+                font-size: 22px !important
+            }
+
+            h2 {
+                font-size: 18px !important
+            }
+
+            h3 {
+                font-size: 16px !important
+            }
+
+            .container {
+                padding: 0 !important;
+                width: 100% !important
+            }
+
+            .content {
+                padding: 0 !important
+            }
+
+            .content-wrap {
+                padding: 10px !important
+            }
+
+            .invoice {
+                width: 100% !important
+            }
+        }
+    </style>
+    <base target="_target">
+    <table style="table-layout:fixed; width:100%; max-width:600px; clear:both !important; margin:0 auto !important"
+        width="100%">
+
+		{{range .Tweets}}
+        <tr>
+            <td style="vertical-align:top; border:1px solid #E2E6E6; padding:5px; border-bottom:none" valign="top">
+
+                <table style="table-layout:fixed; width:100%" width="100%">
+                    <tr>
+
+                        <td style="vertical-align:top; text-align:center; width:60px" valign="top" align="center"
+                            width="60">
+                            <img style="max-width:100%; border-radius:50%; height:48px; min-width:48px; width:48px"
+                                src="{{.User.ProfileImageUrlHttps}}"
+                                height="48" width="48">
+                        </td>
+                        <td style="vertical-align:top" valign="top">
+                            <table cellpadding="0" cellspacing="0" border="0"
+                                style="table-layout:fixed; width:100%; padding-left:5px" width="100%">
+                                <tr>
+                                    <td style="vertical-align:top" valign="top">
+
+                                        <a href="https://twitter.com/{{.User.ScreenName}}/status/{{.Id}}"
+                                            style="color:black; text-decoration:None">
+                                            <strong>{{ .User.Name }}</strong>
+                                            <span>@{{.User.ScreenName}}</span>
+                                            <span style="float:right;">{{. | formatTime }}</span>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="vertical-align:top" valign="top">
+
+                                        <p style="margin-bottom:10px; margin:0; padding-bottom:5px; white-space:pre-wrap">
+{{ .FullText }}    
+										</p>
+
+{{range .ExtendedEntities.Media}}
+<img src="{{.Media_url_https}}"  style="max-width:100%; padding-bottom:5px">
+{{end}}
+
+{{range .Entities.Urls}}
+
+<table style="table-layout:fixed; width:100%; border-radius:12px; border:1px solid #E2E6E6; padding:5px; margin:5px 0"   width="100%">
+    <tr>
+
+        <td style="vertical-align:top" valign="top">
+            <table cellpadding="0" cellspacing="0" border="0"
+                style="table-layout:fixed; width:100%; padding-left:5px"
+                width="100%">
+                <tr>
+                    <td style="vertical-align:top" valign="top">
+                        <p
+                            style="margin-bottom:10px; margin:0; overflow:hidden; text-overflow:inherit; white-space:normal">
+                            <a href="{{.Expanded_url}}"
+                                target="_blank"
+                                style="color:#000; text-decoration:None">
+                                {{.Expanded_url | getTwitterImage}}
+
+                                <strong>{{.Expanded_url}}</strong>
+                            </a>
+                        </p>
+                    </td>
+                </tr>
+
+                
+
+            </table>
+        </td>
+    </tr>
+</table>
+{{end}}
+
+                                    </td>
+                                </tr>
+
+
+                                <td style="vertical-align:top" valign="top">
+                                    <table style="table-layout:fixed; width:100%" width="100%">
+                                        <tr>
+                                            <a href="https://twitter.com/{{.User.ScreenName}}/status/{{.Id}}"
+                                                target="_blank" style="color:#348eda; text-decoration:None">
+                                                <p style="margin-bottom:10px; margin:0">
+
+                                                    <span style="color:#4e555b; margin-right:28px">
+                                                        <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Retweet.png"
+                                                            style="max-width:100%; display:inline; height:16px; padding-top:1px; vertical-align:text-top; width:auto"
+                                                            height="16" valign="text-top" width="auto">
+                                                        <span>{{.RetweetCount}}</span>
+                                                    </span>
+                                                    <span style="color:#4e555b; margin-right:28px">
+                                                        <img src="https://upload.wikimedia.org/wikipedia/commons/c/c9/Twitter_favorite.png"
+                                                            style="max-width:100%; display:inline; height:16px; padding-top:1px; vertical-align:text-top; width:auto"
+                                                            height="16" valign="text-top" width="auto">
+                                                        <span>{{.FavoriteCount}}</span>
+                                                    </span>
+                                                </p>
+                                            </a>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </table>
+                            <a href="https://twitter.com/{{.User.ScreenName}}/"
+                                target="_blank" style="color:#348eda; text-decoration:None"></a>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+		{{end}}
+
+        
+    </table>
+
+
+
+
+
+
+</body>
+
+</html>
+`
